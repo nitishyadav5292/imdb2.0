20a087def1f74f4b84509bb2dc1cedfd350dc802
Nitish Yadav <nitishyadav5292@gmail.com>
2024-05-10 21:21:54 +0530

    Error Handling

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,10 @@
 "use client";
+
 import Image from "next/image";
 
 export default function Results({ results }) {
   return (
-    <div className="text-blue-700">
+    <div className="text-red-700">
       {results.map((result) => (
         <div key={result.id} className="flex flex-col items-center my-4">
           <h2 className="text-xl font-semibold">{result.title}</h2>
