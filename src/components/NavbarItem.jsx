ef3e91afeded24b27b7e48daba001298c9c7d9bd
Nitish Yadav <nitishyadav5292@gmail.com>
2024-05-12 15:53:00 +0530

    Bug fixes

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,19 +1,23 @@
 "use client";
+
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <div
-      className={`m-4 hover:text-amber-600 font-semibold p-2 ${
-        genre && genre === param
-          ? "underline underline-offset-8 decoration decoration-4 decoration-amber-500 rounded-lg"
-          : ""
-      }`}
-    >
-      <Link href={`/?genre=${param}`}>{title}</Link>
-    </div>
+    <Suspense>
+      <div
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+          genre && genre === param
+            ? "underline underline-offset-8 decoration decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+      >
+        <Link href={`/?genre=${param}`}>{title}</Link>
+      </div>
+    </Suspense>
   );
 }
